72dc36874c3897abb079c3891bb40fb279f1f449
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-03 16:55:44 -0600

    mostrando en la pantalla y agregando spinner

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,19 @@
 import {create} from "zustand"
 import {devtools} from 'zustand/middleware'
-import { CryptoCurrency, Pair } from "./types"
+import { CryptoCurrency, CryptoPrice, Pair } from "./types"
 import { getCryptos, fetchCurrentCryptoPrice } from "./services/CryptoService"
 
 type CryptoStore = {
   cryptocurrencies: CryptoCurrency[]
+  result: CryptoPrice
+  loading: boolean
   fetchCrypto: () => Promise<void>
   fetchData: (pair: Pair) => Promise<void>
 }  
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set)=>({
   cryptocurrencies: [],
+  result:{} as CryptoPrice,
     fetchCrypto: async () => {
       const  cryptocurrencies = await getCryptos()
       set(()=>({
@@ -18,9 +21,18 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set)=>({
       }))
 
     },
+    loading: false
+    ,
     fetchData: async (pair) => {
+      
+      set(()=>({
+        loading: true
+      }))
       const result = await fetchCurrentCryptoPrice(pair)
-      console.log(result)
+      set(()=>({
+        result,
+        loading:false
+      }))
       
     }
 })))
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
-import {CryptoCurrencyResponseSchema,CurrencySchema, PairSchema} from '../schema/cripto-schema'
+import {CryptoCurrencyResponseSchema,CryptoPriceSchema,CurrencySchema, PairSchema} from '../schema/cripto-schema'
 import {z} from 'zod'
 
 export type Currency = z.infer<typeof CurrencySchema>
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
 export type Pair = z.infer<typeof PairSchema>
+export type CryptoPrice = z.infer<typeof CryptoPriceSchema>
